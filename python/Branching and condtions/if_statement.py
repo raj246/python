66ea06993039f683d30e4189b8d532dcad242d88
Rajasekhar Reddy <raj@RajasekharsMBP2.attlocal.net>
2022-03-19 17:28:08 -0500

    update python scripts

---
+++ python/Branching and condtions/if_statement.py
@@ -0,0 +1,9 @@
+print("To continue please login in. ")
+
+#Using if statement
+password = input("Please enter your password.")
+if password == "Raj":
+    print("Access granted.")
+else:
+    print("Access denied.")
+
